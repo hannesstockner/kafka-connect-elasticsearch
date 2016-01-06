e89371d350243c049fa2a01d932569c86b29cf90
Hannes Stockner <hannes.stockner@gmail.com>
2016-01-06 09:25:03 +0000

    Replace OpenRecipePublisher with connect-file-source.

--- run_standalone.sh
+++ run_standalone.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
+if [ -z "$DOCKER_IP" ]; then
+  echo "Need to set DOCKER_IP"
+  exit 1
+fi
+
+CONNECT_PROCESS_FILE=connect-standalone.properties
+CONNECT_FILE_SOURCE=connect-file-source.properties
+CONNECT_ES_SINK=connect-elasticsearch-sink.properties
+
+sed -i '' -e "s/bootstrap.servers=.*/bootstrap.servers=$DOCKER_IP:9092/g" $CONNECT_PROCESS_FILE
+sed -i '' -e "s/es.host=.*/es.host=$DOCKER_IP/g" $CONNECT_ES_SINK
+
 CLASSPATH=target/*-jar-with-dependencies.jar
-exec java -Dlog4j.configuration=file:/Users/hannes.stockner/Documents/tools/kafka_2.10-0.9.0.0/config/connect-log4j.properties -cp $CLASSPATH org.apache.kafka.connect.cli.ConnectStandalone connect-standalone.properties connect-elasticsearch-sink.properties
+exec java -cp $CLASSPATH org.apache.kafka.connect.cli.ConnectStandalone $CONNECT_PROCESS_FILE $CONNECT_FILE_SOURCE $CONNECT_ES_SINK
